af308d84e8c58c7ef6343250ed386bfecb963562
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-27 14:12:56 -0700

    Created basic structure of game

--- game.js
+++ game.js
@@ -1,1 +1,45 @@
+var prompt = require('prompt');
+prompt.start();
 
+var spots = {
+    1 : ' ',
+    2 : ' ', 
+    3 : ' ',
+    4 : ' ', 
+    5 : ' ',
+    6 : ' ',
+    7 : ' ',
+    8 : ' ',
+    9 : ' '
+}
+
+var board = spots[1] + ' | ' + spots[2] + ' | ' + spots[3] + '\n' + '---------' + '\n'   
+    +  spots[4] + ' | ' + spots[5] + ' | ' + spots[6] + '\n' + '---------' + '\n'
+    + spots[7] + ' | ' + spots[8] + ' | ' + spots[9] + '\n'; 
+
+function playTurn(number){
+    prompt.get(['number'], (err, result) =>{
+        if(err){
+            console.log(err);
+            return; 
+        }
+        console.log(result.number);
+        var num = result.number;
+        spots[num] = '1';
+        console.log(spots);
+         console.log(board);
+    });
+    
+}
+
+
+
+
+console.log('Enter a number');
+// prompt.get(['number'], function(err, result){
+//     console.log('INPUT', result.number);
+//  })
+
+playTurn();
+
+ 
