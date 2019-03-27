876646378c6e6a983eb329a1e20939991651d7ce
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-27 14:18:15 -0700

    Implemented playable game by input of numbers

--- game.js
+++ game.js
@@ -13,9 +13,6 @@ var spots = {
     9 : ' '
 }
 
-var board = spots[1] + ' | ' + spots[2] + ' | ' + spots[3] + '\n' + '---------' + '\n'   
-    +  spots[4] + ' | ' + spots[5] + ' | ' + spots[6] + '\n' + '---------' + '\n'
-    + spots[7] + ' | ' + spots[8] + ' | ' + spots[9] + '\n'; 
 
 function playTurn(number){
     prompt.get(['number'], (err, result) =>{
@@ -23,11 +20,14 @@ function playTurn(number){
             console.log(err);
             return; 
         }
-        console.log(result.number);
+        console.log(typeof(result.number) , result.number);
         var num = result.number;
         spots[num] = '1';
         console.log(spots);
-         console.log(board);
+         console.log(spots[1] + ' | ' + spots[2] + ' | ' + spots[3] + '\n' + '---------' + '\n'   
+         +  spots[4] + ' | ' + spots[5] + ' | ' + spots[6] + '\n' + '---------' + '\n'
+         + spots[7] + ' | ' + spots[8] + ' | ' + spots[9] + '\n');
+         playTurn();
     });
     
 }
